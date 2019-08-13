88a017154992d0d9ac7952ffb90402fba4ba94ca
mp <mpasdziernik@cognesys.de>
2019-08-13 15:32:49 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject ring-cljsjs "0.1.0"
+(defproject ring-cljsjs "0.2.0-SNAPSHOT"
   :description "Ring middleware to serve assets from Cljsjs packages"
   :url "https://github.com/deraen/ring-cljsjs"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring/ring-core "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ring/ring-core "1.7.1"]]
   :profiles
   {:dev {:dependencies [[cljsjs/react-mdl "1.4.3-0"]
                         [ring/ring-mock "0.2.0"]]}})
